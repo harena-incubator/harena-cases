8b98a479f40e12161cb6443ccf0242b4e6f1972f
CarolineNakazato <carolinenakazato@yahoo.com.br>
2023-08-21 16:33:37 -0300

    update(heart): new images and blocks add

---
+++ health/preparacaoTorax/blocks.js
@@ -0,0 +1,164 @@
+Blockly.Blocks['imagem_gilete'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "gilete",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "1gitele.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 20,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
+
+Blockly.Blocks['imagem_algodao'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "example_image",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "3algodao.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 60,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
+
+Blockly.Blocks['imagem_gazes'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "example_image",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "5gazes.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 120,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
+
+Blockly.Blocks['imagem_cotonete'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "example_image",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "4cotonete.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 200,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
+
+Blockly.Blocks['imagem_alcool'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "example_image",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "2alcool.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 350,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
+Blockly.Blocks['imagem_gel'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "example_image",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "7gel.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 500,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
+Blockly.Blocks['imagem_rad'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "example_image",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "8rad.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 750,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
+Blockly.Blocks['imagem_prepad'] = {
+  init: function () {
+    this.jsonInit({
+        "type": "example_image",
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_image",
+            "src": "6prepad.jpeg",
+            "width": 200,
+            "height": 200,
+            "alt": "*"
+          }
+        ],
+        colour: 950,
+        nextStatement: null,
+        previousStatement: null
+    })
+  }
+}
