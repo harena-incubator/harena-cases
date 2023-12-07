596521db8759649fa3a89d9e1987c24a2b2cc389
CarolineNakazato <carolinenakazato@yahoo.com.br>
2023-12-06 21:56:02 -0300

    update(heart): new tutorial

--- health/heart/tutorial/tutorial3/blocks.js
+++ health/heart/tutorial/tutorial3/blocks.js
@@ -1,3 +1,25 @@
+Blockly.Blocks['numero'] = {
+  init: function () {
+    this.jsonInit({
+      message0: '%1',
+      args0: [
+        {
+          type: 'field_dropdown',
+          name: 'entity',
+          options: [
+            [{src: '1n.png', width: 200, height: 200, alt: 'imagem 1'}, 'imagem1'],
+            [{src: '2n.png', width: 200, height: 200, alt: 'imagem 2'}, 'imagem2'],
+            [{src: '3n.png', width: 200, height: 200, alt: 'imagem 3'}, 'imagem3']
+          ]
+        }
+      ],
+      colour: 20,
+      tooltip: 'num',
+      output: 'numero'
+    })
+  }
+}
+
 Blockly.Blocks['imagem'] = {
   init: function () {
     this.jsonInit({
@@ -7,47 +29,76 @@ Blockly.Blocks['imagem'] = {
           type: 'field_dropdown',
           name: 'entity',
           options: [
-            [{src: 'imagem1.png', width: 200, height: 200, alt: 'imagem 1'}, 'imagem1'],
-            [{src: 'imagem2.png', width: 200, height: 200, alt: 'imagem 2'}, 'imagem2']
+            [{src: '1star.png', width: 200, height: 200, alt: 'imagem 1'}, 'imagem1'],
+            [{src: '2star.png', width: 200, height: 200, alt: 'imagem 2'}, 'imagem2'],
+            [{src: '3star.png', width: 200, height: 200, alt: 'imagem 3'}, 'imagem3']
           ]
         }
       ],
-      message1: 'Numero da imagem: %1',
+      message1: 'Cor da estrela: %1',
       args1: [
         {
           type: 'field_dropdown',
           name: 'entity',
           options: [
-            ['UM', 'img1'],
-            ['Dois', 'img2']
+            ['Amarelo', 'img1'],
+            ['Vermelho', 'img2'],
+            ['Azul', 'img2']
           ]
         }
       ],
-      colour: 80,
+      colour: 40,
       tooltip: 'Img',
       output: 'imagens'
     })
   }
 }
 
-
+Blockly.Blocks['texto'] = {
+  init: function () {
+    this.jsonInit({
+      message0: '%1',
+      args0: [
+        {
+          type: 'field_dropdown',
+          name: 'entity',
+          options: [
+            [{src: '1t.png', width: 200, height: 200, alt: 'imagem 1'}, 'imagem1'],
+            [{src: '2t.png', width: 200, height: 200, alt: 'imagem 2'}, 'imagem2'],
+            [{src: '3t.png', width: 200, height: 200, alt: 'imagem 3'}, 'imagem3']
+          ]
+        }
+      ],
+      colour: 80,
+      tooltip: 'txt',
+      output: 'texto'
+    })
+  }
+}
 Blockly.Blocks['sequence'] = {
   init: function () {
     this.jsonInit({
-      message0: '%1 Imagem  %2 Imagem  ',
+      message0: '%1 Número  %2 Estrela %3 Texto',
       args0: [
         {
         type: 'input_value',
         name: 'VAR',
-        check: 'imagens',
+        check: 'numero',
         },
         {
           type: 'input_value',
           name: 'VAR',
           check: 'imagens',
+        },
+        {
+          type: 'input_value',
+          name: 'VAR',
+          check: 'texto',
         }
       ],
-     colour: 500
+     colour: 500,
+     nextStatement: null,
+     previousStatement: null
     })
   }
 }
